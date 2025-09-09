f574f74a0c6cc058971f5433d74aecbd05462e2a
CharlieSu-111 <charlie.su@beaconfireinc.com>
2025-09-09 11:58:15 -0400

    commit made by feature 2 on DTO

---
+++ src/main/java/com/example/test2/DTO/CarDto.java
@@ -0,0 +1,14 @@
+package com.example.test2.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CarDto {
+    private long id;
+}
